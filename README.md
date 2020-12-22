9ec7647bdfa199854d4a00d59bebb18aa5c0ff5f
Ewout Vonk <dev@ewout.to>
2020-12-22 20:24:15 +0100

    fix: added support for Amazon ECR and private registries + added support for externally logging into the docker registries

--- README.md
+++ README.md
@@ -2,11 +2,23 @@
 
 ## Overview
 
-This repository is intended to be forked. It was created to ease the creation of custom Elasticsearch docker images, specifically for the installation of plugins. It uses Github Actions to automate extending Elasticsearch docker images with a configured set of plugins for a configured set of versions and architectures, and pushes the docker images to Github Packages. This way, nothing needs to happen locally, or on dedicated infrastructure for this process.
+This repository is intended to be forked. It was created to ease the creation of custom Elasticsearch docker images, specifically for the installation of plugins. It by default uses Github Actions to automate extending Elasticsearch docker images with a configured set of plugins for a configured set of versions and architectures, and pushes the docker images by default to Github Packages. This way, nothing needs to happen locally, and no dedicated infrastructure is needed for this process.
 
 Alternatively (if you're using Kubernetes for example), you could also use init containers to achieve the same, see: https://www.elastic.co/guide/en/cloud-on-k8s/current/k8s-init-containers-plugin-downloads.html. This however will make your deployment process slower and introduce a live dependency on the plugins repository each (random) time an Elasticsearch k8s pod gets restarted.
 
-Note: pre-7.8.0, architectures are not taken into account, since for those versions Elasticsearch hasn't published architecture specific images. If this repository is configured to handle any architecture except amd64, it will automatically not build any version pre-7.8.0.
+Note: pre-7.8.0, architectures are not taken into account, since for those versions Elastic hasn't published architecture specific images. If this repository is configured to handle any architecture except amd64, it will automatically not build any version pre-7.8.0.
+
+## Supported Docker Registries
+
+Currently, the supported docker registries are:
+
+* Github Packages (`docker.pkg.github.com`)
+* Amazon ECR (requires configuration of extra environment variables)
+* Private docker registries (requires configuration of extra environment variables as well as 2 externally defined bash functions)
+
+## Pull Requests
+
+Pull requests are accepted for supporting more types of (cloud) docker registries as well as CI systems (for example gitlab workflow, Jenkins pipeline).
 
 ## How It Works
 
@@ -21,11 +33,11 @@ On each push to master, it:
     * step 4: filter the list according to `INCLUDE_FILTER`
     * step 5: filter the list according to `EXCLUDE_FILTER`
   * multiply the list of versions to be processed by the chosen architectures (step 6)
-  * filter out any version-architecture combinations for which a docker image already exists in the Github Packages repository (step 7)
+  * filter out any version-architecture combinations for which a docker image already exists in the docker registry (step 7)
   * build and publish the docker images (for each version-architecture combination chosen), which is comprised of these sub steps:
     * use ConfD to generate the appropriate Dockerfile from `templates/Dockerfile.tmpl` based on the chosen plugins and appropriate `FROM`
     * build the docker image with the correct tag
-    * login to *docker.pkg.github.com*
+    * login to docker registry
     * push the docker image
 
 ## Usage
@@ -48,14 +60,75 @@ Environment variable | Default | Source
 `DRYRUN` | *undefined* | `secrets.DRYRUN`
 `DRYRUN_ASSUME_EXISTING` | *undefined* | `secrets.DRYRUN_ASSUME_EXISTING`
 `VERBOSE` | *undefined* | `secrets.VERBOSE`
+`CUSTOM_BASE_URL_OVERRIDE` | *undefined* | `secrets.CUSTOM_BASE_URL_OVERRIDE`
+`EXTERNAL_LOGIN` | *undefined* | `secrets.EXTERNAL_LOGIN`
+`AWS_ACCESS_KEY_ID` | *undefined* | `secrets.AWS_ACCESS_KEY_ID`
+`AWS_SECRET_ACCESS_KEY` | *undefined* | `secrets.AWS_SECRET_ACCESS_KEY`
+`ECR_AWS_ACCOUNT_ID` | *undefined* | `secrets.ECR_AWS_ACCOUNT_ID`
+`ECR_AWS_REGION` | `eu-west-1` | `secrets.ECR_AWS_REGION`
+`ECR_REPOSITORY_NAME` | `elastic/elasticsearch` | `secrets.ECR_REPOSITORY_NAME`
+`USE_AMAZON_ECR` | *undefined* | `secrets.USE_AMAZON_ECR`
 
 ### Configuration
 
+Configuration happens through text files and/or Github Secrets for setting environment variables. In case you want to deliver the environment variables in a different way than using Secrets, adapt the Github Workflow.
+
+#### External login to docker registry
+
+If you want to handle logging in to the docker registry by yourself before this script is called, set
+
+```
+EXTERNAL_LOGIN=true
+```
+
+as Github Secret. This disables logging into the docker registry in publish-docker-images.sh, irrespective of which type of docker registry is used.
+
+#### Use Amazon ECR
+
+If you want to push the docker images to Amazon ECR, you will have to define a number of Github Secrets, as shown in the below table (all except ECR_AWS_REGION and ECR_REPOSITORY_NAME are required). 
+
+Secret name | Example value
+----------- | -------------
+AWS_ACCESS_KEY_ID | ABCDEFGHIJ1234567890
+AWS_SECRET_ACCESS_KEY | abcdefghijkl/+ABCDEFGHIJKL/+1234567890/+
+ECR_AWS_ACCOUNT_ID | 123456789012
+USE_AMAZON_ECR | true
+ECR_AWS_REGION | eu-west-1
+ECR_REPOSITORY_NAME | elastic/elasticsearch
+
+#### Use private docker registry
+
+If you want to push to any another docker registry, you will need to configure:
+
+```
+CUSTOM_BASE_URL_OVERRIDE=<imagename>
+```
+
+where `<imagename>` is as defined [here](https://docs.docker.com/engine/reference/commandline/tag/) (`An image name is made up of slash-separated name components, optionally prefixed by a registry hostname.`); it should thus exclude the image tag and separating colon.
+
+Additionally, two bash functions need to be defined externally and exported:
+
+```
+funcion custom_docker_registry_login () {
+    ...
+}
+```
+
+```
+funcion custom_docker_image_existence_check () {
+    ...
+}
+```
+
+The functions should accept a `--dryrun` argument, which should cause the function to only output the command which would be executed normally. The functions should output error output to stderr and output normal output to stdout.
+
+Furthermore, `custom_docker_image_existence_check` should accept an argument that states the version-architecture combination that should be checked. It should return a returncode of 0 if the docker image does not exist yet and a returncode higher than 0 if the docker image already exists.
+
 #### Versions To Build
 
-Versions can be selected using an include filter and exclude filter, which are implemented with a regular expression. By default all versions are first included and then excluded (they are both set to `^.*$`), in order to prevent Github Actions to immediately start building after forking this repository.
+Versions can be selected using an include filter and exclude filter, which are implemented by regular expressions. By default all versions are first included and then excluded (they are both set to `^.*$`), in order to prevent Github Actions to immediately start building after forking this repository.
 
-The filters can be configured in `include_filter.txt` and `exclude_filter.txt`, which should have regular expression on the first line and nothing else.
+The filters can be configured in `include_filter.txt` and `exclude_filter.txt`, which should have one regular expression on the first line only.
 
 Suppose you only want to build from version 7 onwards, but want to exclude alphas, betas and release candidates:
 
@@ -73,7 +146,7 @@ Alternatively, `INCLUDE_FILTER` and `EXCLUDE_FILTER` can be delivered using Gith
 
 #### Plugins To Install
 
-Plugins can be configured in `plugins.txt`, by default no plugins are installed, and therefore no docker images are built. `plugins.txt` should contain one plugin name or zip URL per line, for example:
+Plugins can be configured in `plugins.txt`. By default no plugins are installed, and therefore no docker images are built. `plugins.txt` should contain one plugin name or zip URL per line, for example:
 
 ```
 repository-s3
@@ -109,7 +182,7 @@ You can enable debug mode, which will only output the filtering of the versions.
 DEBUG=step3
 ```
 
-Make sure there's no space between step and the number of the step. This needs to be supplied using a Github Secret (or adapt the Github Workflow).
+Make sure there's no space between step and the number of the step. This needs to be supplied using a Github Secret.
 
 ### Dry Run Mode
 
@@ -125,7 +198,7 @@ as a Github Secret. Versions will be fetched and filtered, but these steps will 
 * `docker login`
 * `docker push`
 
-It will also show the `Dockerfile` that was generated. By default it will assume the docker image doesn't exist in the Github Packages docker repo. To make the script assume it does, set this:
+It will also show the `Dockerfile` that was generated. By default it will assume the docker image doesn't exist in the docker registry. To make the script assume it does, set this:
 
 ```
 DRYRUN_ASSUME_EXISTING=true
--- publish-docker-images.sh
+++ publish-docker-images.sh
@@ -4,6 +4,18 @@ function cleanup () {
     rm -f $TMPFILE ${TMPFILE}.stderr ${TMPFILE}.stdout ${TMPFILE}.stderr.* ${TMPFILE}.stdout.*
 }
 
+function is_custom_base_url_overridden () {
+    [ -n "$CUSTOM_BASE_URL_OVERRIDE" ]
+}
+
+function is_login_external () {
+    [ -n "$EXTERNAL_LOGIN" -a "$EXTERNAL_LOGIN" = "true" ]
+}
+
+function use_amazon_ecr () {
+    [ -n "$USE_AMAZON_ECR" -a "$USE_AMAZON_ECR" = "true" ]
+}
+
 function is_verbose_mode () {
     [ -n "$VERBOSE" -a "$VERBOSE" = "true" ]
 }
@@ -76,33 +88,53 @@ function multiply-by-architecture () {
     return 0
 }
 
+function check_github_packages () {
+    local VERSIONARCH="$1"
+    RETURNCODE="1"
+    TMPFILEERR="${TMPFILE}.stderr"
+    TMPFILEOUT="${TMPFILE}.stdout"
+    rm -f $TMPFILEERR $TMPFILEOUT ${TMPFILE}.stderr.* ${TMPFILE}.stdout.*
+    echo "running 'curl --show-error -s -X GET https://docker.pkg.github.com/v2/${GITHUB_REPOSITORY}/elasticsearch/manifests/${VERSIONARCH} -u $GITHUB_ACTOR:$GITHUB_TOKEN | tee -a ${TMPFILE}.stdout.curl | jq '(.errors // []) | map(.code)[]''.." >&4
+    curl --show-error -s -X GET https://docker.pkg.github.com/v2/${GITHUB_REPOSITORY}/elasticsearch/manifests/${VERSIONARCH} -u $GITHUB_ACTOR:$GITHUB_TOKEN 2>${TMPFILE}.stderr.curlerr | tee -a ${TMPFILE}.stdout.curl | jq '(.errors // []) | map(.code)[]' 1>$TMPFILEOUT 2>${TMPFILE}.stderr.jqerr
+    if [ -n "$(cat ${TMPFILE}.stderr.curlerr)" ]; then
+        echo "error: curl failed with error:" >${TMPFILEERR}
+        cat "${TMPFILE}.stderr.curlerr" >>${TMPFILEERR}
+    elif [ -n "$(cat ${TMPFILE}.stderr.jqerr)" ]; then
+        echo "error: jq failed with error:" >${TMPFILEERR}
+        cat "${TMPFILE}.stderr.jqerr" >>${TMPFILEERR}
+        echo "curl output:" >>${TMPFILEERR}
+        cat "${TMPFILE}.stdout.curl" >>${TMPFILEERR}
+    elif [ -n "$(cat $TMPFILEOUT | grep -v -e MANIFEST_UNKNOWN -e NAME_UNKNOWN)" ]; then
+        echo "error: unrecognized error code(s) detected: $(cat $TMPFILEOUT | grep -v -e MANIFEST_UNKNOWN -e NAME_UNKNOWN | sort | uniq | perl -p -e 's#\n#,#;' | perl -p -e 's#,$##;')" >${TMPFILEERR}
+        echo "curl output:" >>${TMPFILEERR}
+        cat "${TMPFILE}.stdout.curl" >>${TMPFILEERR}
+    elif [ -n "$(cat $TMPFILEOUT)" ]; then
+        RETURNCODE="0"
+    fi
+    if [ -e $TMPFILEERR ]; then
+        cat $TMPFILEERR >&2
+        exit 1
+    fi
+    rm -f $TMPFILEERR $TMPFILEOUT ${TMPFILE}.stderr.* ${TMPFILE}.stdout.*
+    return $RETURNCODE
+}
+
+function check_amazon_ecr () {
+    local VERSIONARCH="$1"
+    echo "running 'aws ecr describe-images --repository=${ECR_REPOSITORY_NAME:-elastic/elasticsearch} --image-ids=imageTag=${VERSIONARCH} 1>/dev/null 2>&1'.." >&4
+    ! aws ecr describe-images --repository=${ECR_REPOSITORY_NAME:-elastic/elasticsearch} --image-ids=imageTag=${VERSIONARCH} 1>/dev/null 2>&1
+}
+
 function filter-out-already-existing-custom-es-docker-images () {
     if ! is_dryrun_mode ; then
         cat - | while read VERSIONARCH ; do
-            TMPFILEERR="${TMPFILE}.stderr"
-            TMPFILEOUT="${TMPFILE}.stdout"
-            rm -f $TMPFILEERR $TMPFILEOUT ${TMPFILE}.stderr.* ${TMPFILE}.stdout.*
-            curl --show-error -s -X GET https://docker.pkg.github.com/v2/${GITHUB_REPOSITORY}/elasticsearch/manifests/${VERSIONARCH} -u $GITHUB_ACTOR:$GITHUB_TOKEN 2>${TMPFILE}.stderr.curlerr | tee -a ${TMPFILE}.stdout.curl | jq '(.errors // []) | map(.code)[]' 1>$TMPFILEOUT 2>${TMPFILE}.stderr.jqerr
-            if [ -n "$(cat ${TMPFILE}.stderr.curlerr)" ]; then
-                echo "error: curl failed with error:" >${TMPFILEERR}
-                cat "${TMPFILE}.stderr.curlerr" >>${TMPFILEERR}
-            elif [ -n "$(cat ${TMPFILE}.stderr.jqerr)" ]; then
-                echo "error: jq failed with error:" >${TMPFILEERR}
-                cat "${TMPFILE}.stderr.jqerr" >>${TMPFILEERR}
-                echo "curl output:" >>${TMPFILEERR}
-                cat "${TMPFILE}.stdout.curl" >>${TMPFILEERR}
-            elif [ -n "$(cat $TMPFILEOUT | grep -v -e MANIFEST_UNKNOWN -e NAME_UNKNOWN)" ]; then
-                echo "error: unrecognized error code(s) detected: $(cat $TMPFILEOUT | grep -v -e MANIFEST_UNKNOWN -e NAME_UNKNOWN | sort | uniq | perl -p -e 's#\n#,#;' | perl -p -e 's#,$##;')" >${TMPFILEERR}
-                echo "curl output:" >>${TMPFILEERR}
-                cat "${TMPFILE}.stdout.curl" >>${TMPFILEERR}
-            elif [ -n "$(cat $TMPFILEOUT)" ]; then
-                echo ${VERSIONARCH}
+            if is_custom_base_url_overridden ; then
+                custom_docker_image_existence_check ${VERSIONARCH} && echo ${VERSIONARCH}
+            elif use_amazon_ecr ; then
+                check_amazon_ecr ${VERSIONARCH} && echo ${VERSIONARCH}
+            else
+                check_github_packages ${VERSIONARCH} && echo ${VERSIONARCH}
             fi
-            if [ -e $TMPFILEERR ]; then
-                cat $TMPFILEERR >&2
-                exit 1
-            fi
-            rm -f $TMPFILEERR $TMPFILEOUT ${TMPFILE}.stderr.* ${TMPFILE}.stdout.*
         done
     elif is_dryrun_mode && ! dryrun_assume_existing ; then
         cat -
@@ -113,8 +145,15 @@ function filter-out-already-existing-custom-es-docker-images () {
 function dryrun-filter-out-already-existing-custom-es-docker-images () {
     if is_dryrun_mode ; then
         cat - | while read VERSIONARCH ; do
-            echo "running 'curl -s -X GET https://docker.pkg.github.com/v2/${GITHUB_REPOSITORY}/elasticsearch/manifests/${VERSIONARCH} -u $GITHUB_ACTOR:$GITHUB_TOKEN | jq -r '.''.." >&4
-            curl -s -X GET https://docker.pkg.github.com/v2/${GITHUB_REPOSITORY}/elasticsearch/manifests/${VERSIONARCH} -u $GITHUB_ACTOR:$GITHUB_TOKEN | jq -r '.'
+            if is_custom_base_url_overridden ; then
+                custom_docker_image_existence_check --dryrun ${VERSIONARCH}
+            elif use_amazon_ecr ; then
+                echo "running 'aws ecr describe-images --repository=${ECR_REPOSITORY_NAME:-elastic/elasticsearch} --image-ids=imageTag=${VERSIONARCH} | jq -r '.''.." >&4
+                aws ecr describe-images --repository=${ECR_REPOSITORY_NAME:-elastic/elasticsearch} --image-ids=imageTag=${VERSIONARCH} | jq -r '.'
+            else
+                echo "running 'curl --show-error -s -X GET https://docker.pkg.github.com/v2/${GITHUB_REPOSITORY}/elasticsearch/manifests/${VERSIONARCH} -u $GITHUB_ACTOR:$GITHUB_TOKEN | jq -r '.''.." >&4
+                curl --show-error -s -X GET https://docker.pkg.github.com/v2/${GITHUB_REPOSITORY}/elasticsearch/manifests/${VERSIONARCH} -u $GITHUB_ACTOR:$GITHUB_TOKEN | jq -r '.'
+            fi
         done
     fi
     return 0
@@ -142,11 +181,28 @@ function publish-docker-image () {
         exit 1
     fi
 
-    if is_dryrun_mode ; then
-        echo docker login https://docker.pkg.github.com --username ${GITHUB_REPOSITORY_OWNER} --password-stdin
-    else
-        echo "running 'docker login https://docker.pkg.github.com --username ${GITHUB_REPOSITORY_OWNER} --password-stdin'.." >&4
-        echo $GITHUB_TOKEN | docker login https://docker.pkg.github.com --username ${GITHUB_REPOSITORY_OWNER} --password-stdin || exit 1
+    if ! is_login_external ; then
+        if is_custom_base_url_overridden ; then
+            if is_dryrun_mode ; then
+                custom_docker_registry_login --dryrun ${VERSIONARCH}
+            else
+                custom_docker_registry_login ${VERSIONARCH}
+            fi
+        elif use_amazon_ecr ; then
+            if is_dryrun_mode ; then
+                echo "aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${ECR_AWS_ACCOUNT_ID}.dkr.ecr.${ECR_AWS_REGION:-eu-west-1}.amazonaws.com"
+            else
+                echo "running 'aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${ECR_AWS_ACCOUNT_ID}.dkr.ecr.${ECR_AWS_REGION:-eu-west-1}.amazonaws.com'.." >&4
+                aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${ECR_AWS_ACCOUNT_ID}.dkr.ecr.${ECR_AWS_REGION:-eu-west-1}.amazonaws.com
+            fi
+        else
+            if is_dryrun_mode ; then
+                echo docker login https://docker.pkg.github.com --username ${GITHUB_REPOSITORY_OWNER} --password-stdin
+            else
+                echo "running 'docker login https://docker.pkg.github.com --username ${GITHUB_REPOSITORY_OWNER} --password-stdin'.." >&4
+                echo $GITHUB_TOKEN | docker login https://docker.pkg.github.com --username ${GITHUB_REPOSITORY_OWNER} --password-stdin || exit 1
+            fi
+        fi
     fi
 
     echo "running 'docker push $ES_CUSTOM_IMAGE_URL'.." >&4
@@ -182,9 +238,27 @@ ARCHITECTURE_SUPPORT_REGEX="${ARCHITECTURE_SUPPORT_REGEX:-$(cat architecture_sup
 echo "defining UPSTREAM_BASE_URL.." >&4
 export UPSTREAM_BASE_URL="docker.elastic.co/elasticsearch/elasticsearch"
 echo "defining CUSTOM_BASE_URL.." >&4
-export CUSTOM_BASE_URL="docker.pkg.github.com/${GITHUB_REPOSITORY}/elasticsearch"
+if is_custom_base_url_overridden ; then
+    export CUSTOM_BASE_URL="$CUSTOM_BASE_URL_OVERRIDE"
+elif use_amazon_ecr ; then
+    export CUSTOM_BASE_URL="${ECR_AWS_ACCOUNT_ID}.dkr.ecr.${ECR_AWS_REGION:-eu-west-1}.amazonaws.com/${ECR_REPOSITORY_NAME:-elastic/elasticsearch}"
+else
+    export CUSTOM_BASE_URL="docker.pkg.github.com/${GITHUB_REPOSITORY}/elasticsearch"
+fi
 echo "defining ES_PLUGINS.." >&4
 export ES_PLUGINS="${ES_PLUGINS:-$(cat plugins.txt | perl -p -e 's#\n#,#;' | perl -p -e 's#,$##;')}"
+
+if is_custom_base_url_overridden ; then
+    echo "checking if necessary custom functions are defined.." >&4
+    if ! type custom_docker_registry_login &>/dev/null ; then
+        echo "error: custom function 'custom_docker_registry_login' is not defined, exiting.." >&2
+        exit 1
+    fi
+    if ! type custom_docker_image_existence_check &>/dev/null ; then
+        echo "error: custom function 'custom_docker_image_existence_check' is not defined, exiting.." >&2
+        exit 1
+    fi
+fi
 
 echo "checking if plugins are configured to be installed.." >&4
 if [ -z "${ES_PLUGINS}" ]; then
